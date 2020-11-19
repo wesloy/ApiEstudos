708111709ae3e2dafedb73b4ae80221941e8be22
Wesley Eloy <52499714+wesloy@users.noreply.github.com>
2020-11-19 08:30:09 -0300

    Update ContextFactory.cs

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -10,7 +10,7 @@ namespace Api.Data.Context
         public MyContext CreateDbContext(string[] args)
         {
             //var connectionString = "Server=localhost;Port=3306;Database=Course;Uid=root;Pwd=admin";
-            var connectionString = "Server=UDPCRPDB03;Database=db_Sentinella_Web;User Id=usr_sentinella;Password=#sdMr4@D3sk#;";
+            var connectionString = "Server=#####;Database=db_Sentinella_Web;User Id=usr_sentinella;Password=#####;";
             var optionBuilder = new DbContextOptionsBuilder<MyContext>();
             //optionBuilder.UseMySql(connectionString);
             optionBuilder.UseSqlServer(connectionString);
